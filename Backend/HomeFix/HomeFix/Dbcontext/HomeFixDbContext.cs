fa1271dcf38f36dfffa49ee6842022461d4cf0b3
Yaco-Ibars <yacoibars97@gmail.com>
2023-08-16 19:18:29 -0300

    Creacion DB

---
+++ Backend/HomeFix/HomeFix/Dbcontext/HomeFixDbContext.cs
@@ -0,0 +1,24 @@
+﻿using HomeFix.Model;
+using Microsoft.EntityFrameworkCore;
+using System.CodeDom;
+using System.Reflection;
+
+namespace HomeFix.Dbcontext;
+
+public class HomeFixDbContext : DbContext
+{
+    public HomeFixDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+    }
+    public DbSet<Rol> Roles { get; set; }
+}
---
+++ Backend/HomeFix/HomeFix/Model/Rol.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeFix.Model
+{
+    public class Rol
+    {
+        
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Nombre { get; set; }
+        [MaxLength(150)]
+        public string Descripcion { get; set; }
+    }
+}
