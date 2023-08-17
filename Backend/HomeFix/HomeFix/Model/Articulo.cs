944b30380e48607c070534b01e0a833a4212195c
Yaco-Ibars <yacoibars97@gmail.com>
2023-08-17 18:07:15 -0300

    Update articulos

--- Backend/HomeFix/HomeFix/Model/Articulo.cs
+++ Backend/HomeFix/HomeFix/Model/Articulo.cs
@@ -1,20 +1,28 @@
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HomeFix.Model
 {
     public class Articulo
     {
         public int Id { get; set; }
-        [Required] [MaxLength(50)] public string Nombre { get; set; }
-        [Required] [MaxLength(500)] public string Descripcion { get; set; }
-        [Required] [MaxLength(50)] public string Imagen { get; set; }
-        [Required] [MaxLength(500)] public int Cantidad { get; set; }
+        [Required] [MaxLength(50)] 
+        public string Nombre { get; set; }
+        [Required] [MaxLength(500)] 
+        public string Descripcion { get; set; }
+        [MaxLength(500)]
+        public string Imagen { get; set; }
+        [Required]
+        public int Cantidad { get; set; }
 
         [Required]
-        [Range(0, Double.PositiveInfinity)]
-        public double Precio { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Precio { get; set; }
 
-        public int IdCategoria { get; set; }
-        public int IdMarca { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+        public int MarcaId { get; set; }
+        public Marca Marca { get; set; }
     }
 }
