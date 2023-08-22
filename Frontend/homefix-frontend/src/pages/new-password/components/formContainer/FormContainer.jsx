feaa18e4697053768394028b2f4e2f3a3e458deb
emanuelpps <emanuelpages.ps@gmail.com>
2023-08-22 10:14:02 -0300

    Added password comparison in new-password page

--- Frontend/homefix-frontend/src/pages/new-password/components/formContainer/FormContainer.jsx
+++ Frontend/homefix-frontend/src/pages/new-password/components/formContainer/FormContainer.jsx
@@ -1,12 +1,52 @@
 import React, { useState, useEffect } from "react";
 import "./FormContainer.css";
+import { useDispatch } from 'react-redux';
+import loginService from '../../../../services/login';
+import { setMessage } from '../../../../reducers/messageReducer';
 
 export default function FormContainer() {
+    const [password , setPassword] = useState('');
     const [newPassword, setNewPassword] = useState('');
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+		dispatch(setMessage(null))
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [])
+
+    const handleSubmit = event => {
+		event.preventDefault()
+        if(password === newPassword){
+            if (!(newPassword || password)) {
+                dispatch(setMessage('Rellena los campos faltantes'))
+            } else {
+          console.log('set');
+                setNewPassword('')
+                handlePassword(newPassword)
+            }
+        } else{
+            dispatch(setMessage('La contraseña ingresada no es correcta'))
+        }
+
+	}
+
+    const handlePassword = async (newPassword) => {
+		try {
+			/*const user = await loginService.login({ email, password })
+			dispatch(setToken(`bearer ${user.token}`))
+			window.localStorage.setItem('loggedHomefixUser', JSON.stringify(user))
+			dispatch(setUser(user))*/
+		} catch (error) {
+			dispatch(
+				setMessage('El correo electrónico y/o contraseña no son correctas')
+			)
+		}
+	}
 
   return (
     <div className="newPassword_formContianer">
-      <form >
+      <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="formGroupExampleInput" className="form-label">
             Tu contraseña
@@ -17,6 +57,8 @@ export default function FormContainer() {
             id="password"
             placeholder="Ingresa tu contraseña"
             name="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
           />
         </div>
         <div className="mb-3">
@@ -26,10 +68,11 @@ export default function FormContainer() {
           <input
             type="password"
             className="form-control"
-            id="password"
+            id="newPassword"
             placeholder="Ingresa tu contraseña"
             name="password"
             value={newPassword}
+            onChange={({ target }) => setNewPassword(target.value)}
           />
         </div>
         <div className="login_buttonContainer">
