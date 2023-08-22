72297c22be0e7aa3f217d141f32c8bf3adab69ef
mldamico <matias.lioneldamico@gmail.com>
2023-08-22 12:10:23 -0300

    Admin methods moves to another controller

---
+++ Backend/HomeFix/HomeFix/Controllers/AdminController.cs
@@ -0,0 +1,104 @@
+using HomeFix.DTOs;
+using HomeFix.Model;
+using HomeFix.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeFix.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] 
+public class AdminController : ControllerBase
+{
+    private readonly UserManager<Usuario> _userManager;
+    private readonly TokenService _tokenService;
+
+
+    public AdminController(UserManager<Usuario> userManager, TokenService tokenService)
+    {
+        _userManager = userManager;
+        _tokenService = tokenService;
+    }
+    
+    [Authorize(Roles = "Admin")]
+    [HttpPost("register")]
+    public async Task<IActionResult> Registro(RegistroDto registroDto)
+    {
+        
+        if (await _userManager.Users.AnyAsync(usuario => usuario.UserName == registroDto.UserName))
+        {
+            return BadRequest("Nombre de usuario en uso");
+        }
+        
+        if (await _userManager.Users.AnyAsync(usuario => usuario.Email == registroDto.Email))
+        {
+            return BadRequest("Email en uso");
+        }
+
+        var usuario = new Usuario
+        {
+            UserName = registroDto.UserName,
+            Nombre = registroDto.Nombre,
+            Apellido = registroDto.Apellido,
+            Email = registroDto.Email,
+            ImagenPerfil = registroDto.ImagenPerfil
+        };
+        var result = await _userManager.CreateAsync(usuario, registroDto.Password);
+        
+        if (!result.Succeeded)
+        {
+            return BadRequest("Problema en el registro");
+        }
+
+        var roleResult = await _userManager.AddToRoleAsync(usuario, "Member");
+
+        if (!roleResult.Succeeded)
+        {
+            return BadRequest(roleResult.Errors);
+        }
+
+        return Ok("Usuario creado correctamente");
+
+    }
+    
+    [HttpPost("set-role")]
+    [Authorize(Roles = "Admin")]
+    public async Task<ActionResult> AsignRole(RolUpdateDto editRoleDto)
+    {
+        
+        var user = await _userManager.FindByNameAsync(editRoleDto.UserName);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var userRoles = await _userManager.GetRolesAsync(user);
+        await _userManager.RemoveFromRolesAsync(user, userRoles);
+        
+        var roleResult = await _userManager.AddToRoleAsync(user, editRoleDto.Rol);
+
+        if (!roleResult.Succeeded)
+        {
+            return BadRequest("Error asignando el rol");
+        }
+        
+        return Ok("Rol cambiado correctamente");
+    }
+    
+    [HttpPost("remove-roles")]
+    [Authorize(Roles = "Admin")]
+    public async Task<ActionResult> RemoveRole(RolUpdateDto editRoleDto)
+    {
+        var user = await _userManager.FindByNameAsync(editRoleDto.UserName);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var userRoles = await _userManager.GetRolesAsync(user);
+        await _userManager.RemoveFromRolesAsync(user, userRoles);
+        return Ok("Roles removidos con exito");
+    }
+}
--- Backend/HomeFix/HomeFix/Controllers/CuentaController.cs
+++ Backend/HomeFix/HomeFix/Controllers/CuentaController.cs
@@ -2,15 +2,11 @@ using System.Security.Claims;
 using HomeFix.DTOs;
 using HomeFix.Model;
 using HomeFix.Services;
-using MailKit.Net.Smtp;
-using MailKit.Security;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using MimeKit;
-using MimeKit.Text;
 
 namespace HomeFix.Controllers;
 
@@ -54,48 +50,90 @@ public class CuentaController : ControllerBase
         return Unauthorized();
     }
     
-    [Authorize(Roles = "Admin")]
-    [HttpPost("register")]
-    public async Task<IActionResult> Registro(RegistroDto registroDto)
+    
+
+    
+    [Authorize]
+    [HttpGet]
+    public async Task<ActionResult<UsuarioDto>> GetCurrentUser()
     {
-        
-        if (await _userManager.Users.AnyAsync(usuario => usuario.UserName == registroDto.UserName))
-        {
-            return BadRequest("Nombre de usuario en uso");
-        }
-        
-        if (await _userManager.Users.AnyAsync(usuario => usuario.Email == registroDto.Email))
-        {
-            return BadRequest("Email en uso");
-        }
+        var usuario = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
 
-        var usuario = new Usuario
+        return new UsuarioDto
         {
-            UserName = registroDto.UserName,
-            Nombre = registroDto.Nombre,
-            Apellido = registroDto.Apellido,
-            Email = registroDto.Email,
-            ImagenPerfil = registroDto.ImagenPerfil
+            UserName = usuario.UserName,
+            ImagenPerfil = usuario.ImagenPerfil,
+            Token =  await _tokenService.GenerateToken(usuario),
         };
-        var result = await _userManager.CreateAsync(usuario, registroDto.Password);
-        
-        if (!result.Succeeded)
-        {
-            return BadRequest("Problema en el registro");
-        }
-
-        var roleResult = await _userManager.AddToRoleAsync(usuario, "Member");
-
-        if (!roleResult.Succeeded)
-        {
-            return BadRequest(roleResult.Errors);
-        }
-
-        return Ok("Usuario creado correctamente");
-
     }
+    
+    
+    [AllowAnonymous]
+    [HttpPost("forgot-password")]
+    public async Task<IActionResult> ForgotPassword(ForgotPasswordDto forgotPasswordDto)
+    {
+        Console.WriteLine(forgotPasswordDto.Email);
+        var user = await _userManager.FindByEmailAsync(forgotPasswordDto.Email);
+        
 
-    //El siguiente metodo es de desarrollo solamente para poder crear usuarios sin necesidad de ser un administrador.
+        if (user != null)
+        {
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var baseUrl = _config.GetSection("ClientUrl").Value;
+            var url = $"{baseUrl}/reset?email={user.Email}&token={token}";
+            
+            var request = new EmailDto
+            {
+                To = user.Email,
+                Subject = "Recuperar contraseña",
+                Body = $"Por favor ingrese al siguiente link para recuperar la contraseña. <a href={url}>Click aqui</a>"
+            };
+            
+            _emailService.SendEmail(request);
+         
+            return StatusCode(StatusCodes.Status200OK, "Envio de email para recuperar contraseña enviado. Por favor verifique su direccion de email.");
+        }
+
+        return StatusCode(StatusCodes.Status400BadRequest);
+    }
+    
+    [AllowAnonymous]
+    [HttpPost("reset-password")]
+    public async Task<IActionResult> ResetPassword(ResetPassword resetPassword)
+    {
+        var user = await _userManager.FindByEmailAsync(resetPassword.Email);
+        if (user != null)
+        {
+            var resetPasswordResult = await _userManager.ResetPasswordAsync(user, resetPassword.Token, resetPassword.Password);
+            if (!resetPasswordResult.Succeeded)
+            {
+                foreach (var error in resetPasswordResult.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+
+                return Ok(ModelState);
+            }
+            return StatusCode(StatusCodes.Status200OK, "La contraseña se ha modificado correctamente");
+        }
+
+        return StatusCode(StatusCodes.Status400BadRequest);
+    }
+    
+    [HttpGet("reset-password")]
+    public async Task<IActionResult> ResetPassword(string token, string email)
+    {
+        var user = new ResetPassword {Token = token, Email = email};
+
+        return Ok(new
+        {
+            user
+        });
+    }
+    
+    
+    
+    //Los siguientes metodos son de desarrollo.
     [AllowAnonymous]
     [HttpPost("signup")]
     public async Task<ActionResult<UsuarioDto>> SignUp(RegistroDto registroDto)
@@ -132,128 +170,6 @@ public class CuentaController : ControllerBase
         }
         return BadRequest("Problema en el registro");
     }
-    
-    [Authorize]
-    [HttpGet]
-    public async Task<ActionResult<UsuarioDto>> GetCurrentUser()
-    {
-        var usuario = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
-
-        return new UsuarioDto
-        {
-            UserName = usuario.UserName,
-            ImagenPerfil = usuario.ImagenPerfil,
-            Token =  await _tokenService.GenerateToken(usuario),
-        };
-    }
-
-  
-
-    
-    [AllowAnonymous]
-    [HttpPost("forgot-password")]
-    public async Task<IActionResult> ForgotPassword(ForgotPasswordDto forgotPasswordDto)
-    {
-        Console.WriteLine(forgotPasswordDto.Email);
-        var user = await _userManager.FindByEmailAsync(forgotPasswordDto.Email);
-        
-
-        if (user != null)
-        {
-            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
-            var baseUrl = _config.GetSection("ClientUrl").Value;
-            var url = $"{baseUrl}/reset?email={user.Email}&token={token}";
-            
-            var request = new EmailDto
-            {
-                To = user.Email,
-                Subject = "Recuperar contraseña",
-                Body = $"Por favor ingrese al siguiente link para recuperar la contraseña. <a href={url}>Click aqui</a>"
-            };
-            
-            _emailService.SendEmail(request);
-         
-            return StatusCode(StatusCodes.Status200OK, "Envio de email para recuperar contraseña enviado. Por favor verifique su direccion de email.");
-        }
-
-        return StatusCode(StatusCodes.Status400BadRequest);
-    }
-
-    [HttpGet("reset-password")]
-    public async Task<IActionResult> ResetPassword(string token, string email)
-    {
-        var user = new ResetPassword {Token = token, Email = email};
-
-        return Ok(new
-        {
-            user
-        });
-    }
-    
-    
-    [AllowAnonymous]
-    [HttpPost("reset-password")]
-    public async Task<IActionResult> ResetPassword(ResetPassword resetPassword)
-    {
-        var user = await _userManager.FindByEmailAsync(resetPassword.Email);
-        if (user != null)
-        {
-            var resetPasswordResult = await _userManager.ResetPasswordAsync(user, resetPassword.Token, resetPassword.Password);
-            if (!resetPasswordResult.Succeeded)
-            {
-                foreach (var error in resetPasswordResult.Errors)
-                {
-                    ModelState.AddModelError(error.Code, error.Description);
-                }
-
-                return Ok(ModelState);
-            }
-            return StatusCode(StatusCodes.Status200OK, "La contraseña se ha modificado correctamente");
-        }
-
-        return StatusCode(StatusCodes.Status400BadRequest);
-    }
-    
-    [HttpPost("set-role")]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
-    public async Task<ActionResult> AsignRole(RolUpdateDto editRoleDto)
-    {
-        
-        var user = await _userManager.FindByNameAsync(editRoleDto.UserName);
-        if (user == null)
-        {
-            return NotFound();
-        }
-
-        var userRoles = await _userManager.GetRolesAsync(user);
-        await _userManager.RemoveFromRolesAsync(user, userRoles);
-        
-        var roleResult = await _userManager.AddToRoleAsync(user, editRoleDto.Rol);
-
-        if (!roleResult.Succeeded)
-        {
-            return BadRequest("Error asignando el rol");
-        }
-        
-        return Ok("Rol cambiado correctamente");
-    }
-    
-    [HttpPost("remove-roles")]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
-    public async Task<ActionResult> RemoveRole(RolUpdateDto editRoleDto)
-    {
-        var user = await _userManager.FindByNameAsync(editRoleDto.UserName);
-        if (user == null)
-        {
-            return NotFound();
-        }
-
-        var userRoles = await _userManager.GetRolesAsync(user);
-        await _userManager.RemoveFromRolesAsync(user, userRoles);
-        return Ok("Roles removidos con exito");
-    }
-
-    
     [HttpPost("test")]
     public  IActionResult TestEmail(EmailDto request)
     {
