d901bc0e9bc76dca180993bc951dad1d4b4ce795
emanuelpps <emanuelpages.ps@gmail.com>
2023-08-23 14:29:50 -0300

    added styles in new-password page

--- Frontend/homefix-frontend/src/pages/new-password/components/formContainer/FormContainer.jsx
+++ Frontend/homefix-frontend/src/pages/new-password/components/formContainer/FormContainer.jsx
@@ -1,86 +1,118 @@
 import React, { useState, useEffect } from "react";
 import "./FormContainer.css";
-import { useDispatch } from 'react-redux';
-import loginService from '../../../../services/login';
-import { setMessage } from '../../../../reducers/messageReducer';
+import { useDispatch } from "react-redux";
+import loginService from "../../../../services/login";
+import { setMessage } from "../../../../reducers/messageReducer";
+import showPasswordIcon from "../../../../assets/image/contraseña_mostrar.png";
+import hidePasswordIcon from "../../../../assets/image/contraseña_ocultar.png";
 
 export default function FormContainer() {
-    const [password , setPassword] = useState('');
-    const [newPassword, setNewPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
 
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-		dispatch(setMessage(null))
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [])
+  useEffect(() => {
+    dispatch(setMessage(null));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    const handleSubmit = event => {
-		event.preventDefault()
-        if(password === newPassword){
-            if (!(newPassword || password)) {
-                dispatch(setMessage('Rellena los campos faltantes'))
-            } else {
-          console.log('set');
-                setNewPassword('')
-                handlePassword(newPassword)
-            }
-        } else{
-            dispatch(setMessage('La contraseña ingresada no es correcta'))
-        }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (password === newPassword) {
+      if (!(newPassword || password)) {
+        dispatch(setMessage("Rellena los campos faltantes"));
+      } else {
+        console.log("set");
+        setNewPassword("");
+        handlePassword(newPassword);
+      }
+    } else {
+      dispatch(setMessage("La contraseña ingresada no es correcta"));
+    }
+  };
 
-	}
-
-    const handlePassword = async (newPassword) => {
-		try {
-			/*const user = await loginService.login({ email, password })
+  const handlePassword = async (newPassword) => {
+    try {
+      /*const user = await loginService.login({ email, password })
 			dispatch(setToken(`bearer ${user.token}`))
 			window.localStorage.setItem('loggedHomefixUser', JSON.stringify(user))
 			dispatch(setUser(user))*/
-		} catch (error) {
-			dispatch(
-				setMessage('El correo electrónico y/o contraseña no son correctas')
-			)
-		}
-	}
+    } catch (error) {
+      dispatch(
+        setMessage("El correo electrónico y/o contraseña no son correctas")
+      );
+    }
+  };
+
+  const passwordHandler = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <div className="newPassword_formContianer">
-      <form onSubmit={handleSubmit}>
+      <form className="newPassword_form" onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="formGroupExampleInput" className="form-label">
-            Tu contraseña
+          <label htmlFor="password" className="form-label">
+            Contraseña
           </label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            placeholder="Ingresa tu contraseña"
-            name="password"
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="formGroupExampleInput" className="form-label">
-            Ingresá nuevamente tu contraseña
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="newPassword"
-            placeholder="Ingresa tu contraseña"
-            name="password"
-            value={newPassword}
-            onChange={({ target }) => setNewPassword(target.value)}
-          />
-        </div>
-        <div className="login_buttonContainer">
-          <div className="login_buttonPosition">
-            <button type="submit" className="btn btn-dark">
-              Modificar
+          <div className="input-group">
+            <input
+              type={showPassword ? "text" : "password"}
+              className="form-control formInput formInputPassword pt-3"
+              id="password"
+              placeholder="Ingresa tu contraseña"
+              name="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button
+              class="showPasswordImage-button border border-opacity-50 px-2"
+              type="button"
+              id="button-addon2"
+              onClick={passwordHandler}
+            >
+              <img
+                className="showPasswordImage "
+                src={showPassword ? showPasswordIcon : hidePasswordIcon}
+                alt=""
+              />
             </button>
           </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Ingresá nuevamente tu contraseña
+          </label>
+          <div className="input-group">
+            <input
+              type={showPassword ? "text" : "password"}
+              className="form-control formInput formInputPassword pt-3"
+              id="password"
+              placeholder="Ingresa tu contraseña"
+              name="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button
+              class="showPasswordImage-button border border-opacity-50 px-2"
+              type="button"
+              id="button-addon2"
+              onClick={passwordHandler}
+            >
+              <img
+                className="showPasswordImage "
+                src={showPassword ? showPasswordIcon : hidePasswordIcon}
+                alt=""
+              />
+            </button>
+          </div>
+        </div>
+        <div className="newPassword_buttonContainer">
+          <button type="submit" className="btn btn-dark submitButton newPassword_buttonPosition pt-2">
+            Enviar
+          </button>
         </div>
       </form>
     </div>
