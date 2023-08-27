c596800fb52564d7d3134645c3b125b82fae5603
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-08-26 23:50:25 -0400

    Navbar modified to highlight actual page button

---
+++ Frontend/homefix-frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,54 @@
+import './NavBar.css'
+import summaryIcon from '../../assets/image/gg_loadbar-doc.svg'
+import usersIcon from '../../assets/image/ph_users-fill.svg'
+import inventoryIcon from '../../assets/image/solar_box-bold.svg'
+import reportIcon from '../../assets/image/akar-icons_statistic-up.svg'
+import { useNavigate } from 'react-router-dom'
+
+export default function NavBar({ page }) {
+	const navigate = useNavigate()
+
+	return (
+		<div className="navBar-container">
+			<h2 className="mt-5">Menú</h2>
+			<div className="button-menu-list">
+				<button
+					type="button"
+					onClick={() => navigate('/')}
+					className="button button-reset"
+					style={page === 'summary' && { backgroundColor: '#FBAE43' }}
+				>
+					<img src={summaryIcon} alt="summary icon" />
+					Resumen
+				</button>
+				<button
+					type="button"
+					onClick={() => navigate('/')}
+					className="button button-reset"
+					style={page === 'users' && { backgroundColor: '#FBAE43' }}
+				>
+					<img src={usersIcon} alt="users icon" />
+					Usuarios
+				</button>
+				<button
+					type="button"
+					onClick={() => navigate('/')}
+					className="button button-reset"
+					style={page === 'inventory' && { backgroundColor: '#FBAE43' }}
+				>
+					<img src={inventoryIcon} alt="inventory icon" />
+					Inventario
+				</button>
+				<button
+					type="button"
+					onClick={() => navigate('/')}
+					className="button button-reset"
+					style={page === 'report' && { backgroundColor: '#FBAE43' }}
+				>
+					<img src={reportIcon} alt="report icon" />
+					Informe
+				</button>
+			</div>
+		</div>
+	)
+}
---
+++ Frontend/homefix-frontend/src/pages/dashboard-homepage/layout/Layout.jsx
@@ -0,0 +1,20 @@
+import './Layout.css'
+import NavBar from '../../../components/NavBar/NavBar'
+import HeaderBar from '../../../components/HeaderBar/HeaderBar'
+import DashboardResume from '../components/dashboardResume/DashboardResume'
+import TopProductsContainer from '../components/topProductsContainer/TopProductsContainer'
+import SalesChartsContainer from '../components/salesChartsContainer/SalesChartsContainer'
+import TitleContainer from '../components/titleContainer/TitleContainer'
+
+export default function Layout() {
+	return (
+		<div className="dashboard-layout">
+			<HeaderBar />
+			<NavBar page="summary" />
+			<TitleContainer />
+			<DashboardResume />
+			<TopProductsContainer />
+			<SalesChartsContainer />
+		</div>
+	)
+}
