3315bba0bbd78070c593a1c0247e9119bdd21b3a
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-09-14 15:33:20 -0400

    build ui

--- Frontend/homefix-frontend/src/pages/dashboard-homepage/components/salesChartsContainer/SalesChartsContainer.jsx
+++ Frontend/homefix-frontend/src/pages/dashboard-homepage/components/salesChartsContainer/SalesChartsContainer.jsx
@@ -78,7 +78,7 @@ export default function SalesChartsContainer() {
 	return (
 		<div className="sales-chart-container">
 			<div className="chart-title">
-				<h3>Ventas (últimos 6 meses)</h3>
+				<h3>Ventas de últimos meses</h3>
 			</div>
 			<div className="bar-chart-container">
 				<Bar options={options} data={data} />
